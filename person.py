3072ce5326d85aff9f4a19205955f66c5a135561
stratopedarx <stratopedarx.lobanov@yandex.ru>
2016-03-01 10:34:32 +0300

    Lutz

--- person.py
+++ person.py
@@ -13,12 +13,44 @@ class Person:
         self.pay = int(self.pay * (1 + percent))
 
     def __str__(self):
-        return '[Person: %s -> %s]' % (self.name, self.pay)
+        return '[Person: %s -> %s as %s]' % (self.name, self.pay, self.job)
 
+"""
 class Manager(Person):
+    def __init__(self, name, pay):
+        Person.__init__(self, name, "manager", pay)
+
     def giveRaise(self, percent, bonus=.10):
         Person.giveRaise(self, percent + bonus)
+"""
 
+class Manager:
+    def __init__(self, name, pay):
+        self.person = Person(name, 'mgr', pay)
+
+    def giveRaise(self, percent, bonus=.10):
+        self.person.giveRaise(percent + bonus)
+
+    def __getattr__(self, attr):
+        return getattr(self.person, attr)
+
+    def __str__(self):
+        return str(self.person)
+
+class Department:
+    def __init__(self, *args):
+        self.members = list(args)
+
+    def addMember(self, person):
+        self.members.append(person)
+
+    def giveRaise(self, percent):
+        for person in self.members:
+            person.giveRaise(percent)
+
+    def showAll(self):
+        for person in self.members:
+            print person
 
 
 if __name__ == '__main__':
@@ -30,3 +62,13 @@ if __name__ == '__main__':
     print sue.pay
     print sue
     print bob
+    tom = Manager('Tom Lower', 50000)
+    tom.giveRaise(.10)
+    print tom.lastName()
+    print tom
+    print "=" * 80
+
+    development = Department(bob, sue)
+    development.addMember(tom)
+    development.giveRaise(.50)
+    development.showAll()
